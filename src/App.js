19fae2d311faa68d7ee3ff2a5949326e503ceaba
simple-sifu <kattyntommy@gmail.com>
2020-09-11 21:47:58 -0400

    add componentDidMount

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ class App extends React.Component {
        }
       ]
     };
+
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
   }
 
   render() {
