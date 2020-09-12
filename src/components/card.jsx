04abb1dc0502239a221b30be96cdf3e264332625
simple-sifu <kattyntommy@gmail.com>
2020-09-11 23:54:45 -0400

    added images and email.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,7 +4,8 @@ import './styles.css';
 export const Card = ({monster}) => (
     <div className='card-container'>
         <img alt="monster" src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
-        <h1>{monster.name}</h1>
+        <h2>{monster.name}</h2>
+        <p>{monster.email}</p>
 
     </div>
 )
