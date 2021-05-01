cc10d5c1ac8002fe734b560b3c802e2833e53f96
Tommy Han <tommy_han1@intuit.com>
2021-05-01 18:29:21 -0400

    added id so that React knows how to update the right component that changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export function App() {
-    return (
-        <div>
-            <h1>Welcome { new Date().toString() } </h1>
-        </div>
-    );
-}
+export class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            monsters: [
+                {
+                    name: 'Frankenstein',
+                    id: 'asc1'
+                },
+                {
+                    name: 'Dracula',
+                    id: 'asc2'
+                },
+                {
+                    name: 'Zombie',
+                    id: 'asc3'
+                }
+            ]
+        };
+    }
+
+    render(){
+        return (
+            <div className='App'>
+                {
+                    this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+                }
+            </div>
+        )
+    }
+
+
+} 
