14d4c407b3b7a53a8d26373d7c648d845e69bb1b
Tommy Han <tommy_han1@intuit.com>
2021-05-01 19:49:40 -0400

    modified webpack to allow css to load

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss$/,
+                test: /\.(scss|css)$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
