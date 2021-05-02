eca354a017b1dcd9d5e275723aa29fa3bf267b07
Tommy Han <tommy_han1@intuit.com>
2021-05-02 02:48:50 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,10 @@ export class App extends Component {
           .then(users => this.setState({ monsters: users}))
     }
 
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value})
+    }
+
     render(){
 
         const { monsters, searchField  } = this.state;
@@ -26,7 +30,7 @@ export class App extends Component {
             <div className='App'>
                 <SearchBox
                     placeholder='search monsters'
-                    handleChange={e => this.setState({ searchField: e.target.value})}
+                    handleChange={this.handleChange }
                 />
                 <CardList monsters={filteredMonsters}/>
             </div>
