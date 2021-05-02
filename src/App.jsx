a07d1420e721b0aa9371c7387c8c5a2f4e00422e
Tommy Han <tommy_han1@intuit.com>
2021-05-02 02:29:59 -0400

    componentize searchbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 export class App extends Component {
 
@@ -25,8 +26,10 @@ export class App extends Component {
         console.log("filteredMonsters -", filteredMonsters);
         return (
             <div className='App'>
-                <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value} )
-            } />
+                <SearchBox
+                    placeholder='search monsters'
+                    handleChange={e => this.setState({ searchField: e.target.value})}
+                />
                 <CardList monsters={filteredMonsters}/>
             </div>
         )
